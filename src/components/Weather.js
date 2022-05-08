6c603982921ccf059ebde2546b7611db65d1a869
B_Yoda04 <gerardmatos1990@gmail.com>
2022-05-08 16:19:52 -0500

    08/05/2022

---
+++ src/components/Weather.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import useWeather from '../hooks/useHTTP';
+
+const Weather = () => {
+
+    const [temperature, setTemperature] = useState(true)
+    const {name,country,description,speed,all,pressure,icon,c,f} = useWeather()
+
+    return (
+        
+        <div className='container'>
+
+            <h2>Weather App</h2>
+            <div>
+                <p>City and country: </p><p>{name} {country}</p>
+            </div>
+            <div>
+                <p>Time description: </p><p>{description}</p>
+            </div>
+            <div className='time-description'>
+                <div>
+                    <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
+                </div>
+                <div className='time-container'>
+                    <div>
+                        <p>wind speed: </p><p>{speed}</p>
+                    </div>
+                    <div>
+                        <p>clouds: </p><p>{all}%</p>
+                    </div>
+                    <div>
+                        <p>pressure: </p><p>{pressure}mb</p>
+                    </div>
+                </div>
+            </div>
+            {temperature? <div><p>Temperature: </p><p>{c}°C</p></div> : <div><p>Temperature: </p><p>{f}°F</p></div>}
+            <button onClick={()=>setTemperature(!temperature)}>{temperature? "°F?": "°C?"}</button>
+
+        </div>
+        
+    );
+};
+
+export default Weather;
---
+++ src/hooks/useHTTP.js
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const useWeather = () => {
+
+    const [name,setName] = useState()
+    const [country,setCountry] = useState()
+    const [description,setDescription] = useState()
+    const [speed,setSpeed] = useState()
+    const [all,setAll] = useState()
+    const [pressure,setPressure] = useState()
+    const [icon,setIcon] = useState()
+    const [c,setC] = useState()
+    const [f,setF] = useState()
+
+    useEffect(()=>{
+
+        function success(pos) {
+          let crd = pos.coords;
+          axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=d4810d6ce55ced2a72644b63f1b4332b`)
+            .then(res => {
+                
+                console.log(res !== undefined? "cargando" : "cargado");
+                setName(res.data.name)
+                setCountry(res.data.sys.country)
+                setDescription(res.data.weather[0].description)
+                setSpeed(res.data.wind.speed)
+                setAll(res.data.clouds.all)
+                setPressure(res.data.main.pressure)
+                setIcon(res.data.weather[0].icon)
+                setC(((res.data.main.temp) - 273.15).toFixed(2))
+                setF(((18/10) * ((res.data.main.temp) - 273.15) + 32).toFixed(2))
+              
+            })
+            .catch(error => console.log(error))
+        };
+              
+        function err(er) {
+          console.log(er.message); 
+        };
+                
+        navigator.geolocation.getCurrentPosition(success, err);
+    
+      },[])
+
+    return {name,country,description,speed,all,pressure,icon,c,f}
+};
+
+export default useWeather;
